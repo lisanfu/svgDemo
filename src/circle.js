18db0f7f05d216b6971897517c2f2a473606f816
李复员 <lifuyuan@huohua.cn>
2020-03-14 11:11:31 +0800

    修改

--- src/circle.js
+++ src/circle.js
@@ -1,30 +1,29 @@
-SVG.Circle = function Circle(){
-    this.constructor.call(this,SVG.create('circle'));
-
-}
+SVG.Circle = function Circle() {
+  this.constructor.call(this, SVG.create("circle"));
+};
 
 //inherit from SVG.Shape
 
 SVG.Circle.prototype = new SVG.Shape();
 //add circle-specific function
-SVG.Utils.merge(SVG.Circle,{
-    move: function(x,y){
-        this.attrs.x = x;
-        this.attrs.y = y;
-        this.center();
-        return this;
-    },
-    size: function(w,h){
-        this.attr('r', w / 2);
-        this.center();
+SVG.Utils.merge(SVG.Circle, {
+  move: function(x, y) {
+    this.attrs.x = x;
+    this.attrs.y = y;
+    this.center();
+    return this;
+  },
+  size: function(w, h) {
+    this.attr("r", h / 2);
+    // this.attr("r", w / 2);
+    this.center();
 
-        return this;
-    },
-    //private center
-    center: function(cx,cy){
-        var r = this.attrs.r || 0;
-        this.attr('cx',cx || ((this.attrs.x || 0) + r));
-        this.attr('cy',cy || ((this.attrs.y || 0) + r));
-    }
+    return this;
+  },
+  //private center
+  center: function(cx, cy) {
+    var r = this.attrs.r || 0;
+    this.attr("cx", cx || (this.attrs.x || 0) + r);
+    this.attr("cy", cy || (this.attrs.y || 0) + r);
+  }
 });
-
